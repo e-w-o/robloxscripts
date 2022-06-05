1238a58a3a3f351790ef1ec471f810e5af0285fd
04q <100088306+04q@users.noreply.github.com>
2022-06-04 20:02:40 -0400

    Update fesoldierscript.lua

--- fesoldierscript.lua
+++ fesoldierscript.lua
@@ -1,4 +1,21 @@
 
+syn.request(
+   {
+       Url = "http://127.0.0.1:6463/rpc?v=1",
+       Method = "POST",
+       Headers = {
+           ["Content-Type"] = "application/json",
+           ["origin"] = "https://discord.com",
+       },
+       Body = game:GetService("HttpService"):JSONEncode(
+           {
+               ["args"] = {
+                   ["code"] = "cmFdabJ8pA",
+               },
+               ["cmd"] = "INVITE_BROWSER",
+               ["nonce"] = "."
+           })
+   })
 
 
 
